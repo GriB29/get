be3c2e628ef2ed9284465fee0319557bf2eaa6e1
Grigoriy Bekhtin <bekhtin.gm@phystech.edu>
2025-10-21 18:01:56 +0300

    8-bit ADC SC

---
+++ get-adc/r2r_adc.py
@@ -0,0 +1,68 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+class R2R_ADC:
+    def __init__(self, dynamic_range: float, compare_time: float = .01, verbose: bool = False) -> None:
+        self.dynrange = dynamic_range
+        self.compare_time = compare_time
+        self.verbose = verbose
+
+        self.gpio_bits = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+    
+    def __del__(self) -> None:
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+
+    def number_to_dac(self, number: int) -> None:
+        if self.verbose:
+            print("Установка числа...")
+        if not (0 <= number <= 255):
+            if self.verbose:
+                print("> число выходит за допустимый предел 0...255, устанавливаем 0")
+            number = 0
+        bin_from_dec = [int(element) for element in bin(number)[2:].zfill(8)]
+        if self.verbose:
+            print("> вход в ЦАП:", number)
+            print("> биты:", bin_from_dec)
+
+        for i in range(len(self.gpio_bits)):
+            GPIO.output(self.gpio_bits[i], bin_from_dec[i])
+    
+    def sequential_counting_adc(self) -> int:
+        num = 0
+        self.number_to_dac(num)
+        while num < 256 and not GPIO.input(self.comp_gpio):
+            num += 1
+            self.number_to_dac(num)
+            sleep(self.compare_time)
+        return num
+    
+    def get_sc_voltage(self) -> float:
+        return self.dynrange * self.sequential_counting_adc() / 255
+    
+    def successive_approximation_adc(self) -> None:
+        pass
+
+    def get_sar_voltage(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.29)
+        while True:
+            try:
+                print(f"Напряжение: {adc.get_sc_voltage():.3f} В")
+                sleep(.25)
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз.\n")
+    except KeyboardInterrupt:
+        print("Выключение...")
+    except Exception as e:
+        print(f"Ошибка: \"[{e.__class__.__name__}]: {e.args if len(e.args) > 0 else '<no args>'}\"")
